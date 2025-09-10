e806352d34a0389d036470e50251352f7f0dc11e
Bishobos <mps.kajkage@gmail.com>
2025-09-10 11:00:34 +0200

    Mostly works

--- src/Battleship/BattleshipMain.java
+++ src/Battleship/BattleshipMain.java
@@ -1,4 +1,14 @@
 package Battleship;
 
 public class BattleshipMain {
-}
+    public static void main(String[] args) {
+
+        ship myShip = new ship(new int[]{1, 2, 3});
+
+        while(myShip.hits<3){
+            myShip.checkHit();
+        }
+        System.out.println("You've won!");
+        }
+    }
+
--- src/Battleship/ship.java
+++ src/Battleship/ship.java
@@ -1,4 +1,34 @@
 package Battleship;
 
 public class ship {
+    boolean[] placement = new boolean[7];
+    int hits = 0;
+
+    public ship() {
+        this.placement[6] = true;
+        this.placement[5] = true;
+        this.placement[4] = true;
+    }
+
+    public ship(int[] placement){
+        this.placement[placement[0]] = true;
+        this.placement[placement[1]] = true;
+        this.placement[placement[2]] = true;
+    }
+
+    public void checkHit(){
+        int userInput = GameHelper.getUserInput();
+
+        boolean hit = this.placement[userInput];
+
+        if(hit){
+            this.placement[userInput] = false;
+            this.hits++;
+            System.out.println("You hit!");
+        }if(!hit){
+            System.out.println("You missed!");
+        }
+
+    }
+
 }
