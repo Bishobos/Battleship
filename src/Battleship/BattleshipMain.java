d574e898aaf0b8bad2832d653f9cd9c2bbcd6fdb
Bishobos <mps.kajkage@gmail.com>
2025-09-10 11:06:47 +0200

    works, may need so prettying up

--- src/Battleship/BattleshipMain.java
+++ src/Battleship/BattleshipMain.java
@@ -5,10 +5,17 @@ public class BattleshipMain {
 
         ship myShip = new ship(new int[]{1, 2, 3});
 
+        System.out.println("Welcome to Battleship!\n" +
+                "Enemy ship is hiding within positions 0-6\n" +
+                "Sink it by hitting all 3 positions!");
+        int shots = 0;
         while(myShip.hits<3){
+            shots++;
             myShip.checkHit();
+            System.out.println("You've shot " + shots + " times!");
+
         }
-        System.out.println("You've won!");
+        System.out.println("\n=== Congratulations, you've won! ===");
         }
     }
 
